b12d608a09581109f2547e5e475c813e735baf3a
hiamthach <dreon1008@gmail.com>
2023-07-21 18:03:34 +0700

    fix bug

--- src/config/helpers/image.helper.ts
+++ src/config/helpers/image.helper.ts
@@ -1,3 +1,9 @@
+import { FILE_UPLOAD_LIMIT } from '@/config/constants/general.const';
+
 export const generateBlobUrl = (file: File) => {
   return URL.createObjectURL(file);
+};
+
+export const checkImageSize = (file: File) => {
+  return file.size / 1024 / 1024 > FILE_UPLOAD_LIMIT;
 };
